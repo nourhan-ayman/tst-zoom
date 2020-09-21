0a432bb64fe3518e45459cd6202f5e76bfa57e11
nour <norhan.a@blue-developments.team>
2020-09-21 13:09:02 +0200

    first commit

---
+++ wp-content/plugins/video-conferencing-with-zoom-api/templates/fragments/content.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This template can be overridden by copying it to yourtheme/video-conferencing-zoom/fragments/content.php.
+ *
+ * @author      Deepen Bajracharya (CodeManas)
+ * @created     3.0.0
+ */
+?>
+
+<div class="deepn-zvc-single-description">
+    <h3><?php the_title(); ?></h3>
+	<?php the_content(); ?>
+</div>
